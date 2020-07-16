8843cc437c5c206f8fd2eee3ff466361651c8618
gzhanl <gzlianghan@163.com>
2020-07-16 16:06:54 +0800

    update 20200716

---
+++ README.md
@@ -0,0 +1,12 @@
+## 20200406 新增
+获取个股历史资金流向    （eastmoney.com）  
+get_hist_capital_flow  
+修改：
+1.stock\trading.py 
+2.stock\cons.py 
+3.__init__.py
+
+
+计划：
+增加 板块资金
+http://data.eastmoney.com/bkzj/hy.html
---
+++ stock/bk_capital_flow_test.py
@@ -0,0 +1,11 @@
+import tushare as ts
+import pandas as pd
+
+pd.set_option('display.max_rows',100)
+pd.set_option('display.max_columns',50)
+pd.set_option('display.width',1000)
+
+
+
+res=ts.get_bk_capital_flow('433')
+print(res)
