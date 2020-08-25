05932a269162cc277a2ac33b468dde82e6819d02
gzhanl <gzlianghan@163.com>
2020-08-25 13:48:00 +0800

    202008152008215_1347

--- data chart/nbfbk_chart_bokeh.py
+++ data chart/nbfbk_chart_bokeh.py
@@ -9,12 +9,11 @@ pd.set_option('display.width',1000)
 
 
 """
-酿酒行业:477   医药制造:465  家电行业:465   银行:475  电子元件:459  食品饮料:438  保险:474  机械行业:545  医疗行业: 727  
+酿酒行业:477   医药制造:465  家电行业:456   银行:475  电子元件:459  食品饮料:438  保险:474  机械行业:545  医疗行业: 727  
 券商信托:473   软件服务:737  汽车行业:481   房地产:451  水泥建材:424 旅游酒店:485
 """
-
 # 獲取data
-BK_Code='465'
+BK_Code='545'
 
 df_nbfbk_hist_flow=ts.get_nbfbk_hist_capital_flow(BK_Code)
 print(df_nbfbk_hist_flow)
@@ -24,9 +23,9 @@ print(df_nbfbk_hist_flow)
 X_Date_value=df_nbfbk_hist_flow['date']
 X_Date_value=pd.to_datetime(X_Date_value)
 
-Y_znzjb_value=df_nbfbk_hist_flow['znzjb'].astype(float)
-Y_cgbkb_value=df_nbfbk_hist_flow['cgbkb'].astype(float)*10
-Y_cgsz_value=df_nbfbk_hist_flow['cgsz'].astype(float)*0.000000000001
+Y_znzjb_value=df_nbfbk_hist_flow['znzjb'].astype(float)*10
+Y_cgbkb_value=df_nbfbk_hist_flow['cgbkb'].astype(float)*100
+Y_cgsz_value=df_nbfbk_hist_flow['cgsz'].astype(float)*10
 
 # output to static HTML file
 output_file("lines.html")
@@ -36,7 +35,7 @@ p = figure(tools="hover,pan,box_zoom,reset,save",title="simple line example", x_
 
 # add a line renderer with legend and line thickness
 p.line(X_Date_value, Y_znzjb_value, legend_label="持股占北向资金比.", line_width=4,color='red')
-#p.line(X_Date_value, Y_cgbkb_value, legend_label="今日持股占板块比.", line_width=4)
+p.line(X_Date_value, Y_cgbkb_value, legend_label="今日持股占板块比.", line_width=4,color='blue')
 #p.line(X_Date_value, Y_cgsz_value, legend_label="今日持股市值.", line_width=4,color='green')
 # show the results
 show(p)
